9d701826b5a480b17a4379a62a78c0e6cc50a08c
Md Shakil Chowdhury <ms181161@gmail.com>
2022-04-05 00:34:01 +0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <Nav/>
+    <Hero />
+
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+     />
+    <SmallCardDisplay 
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import {largeCardSections,smallCardSections} from "@/assets/data.js"
+export default {
+  data(){
+    return{
+      largeCardInfo:largeCardSections,
+      smallCardInfo:smallCardSections
+    }
+  }
+
+};
+</script>
+<style scoped>
+
+</style>
